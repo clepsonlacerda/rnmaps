a85f9521b3a6c98c177beea700963bab441536d9
Clepson Lacerda <clepsonsantos9@gmail.com>
2024-06-13 14:35:59 -0300

    get current position

--- App.tsx
+++ App.tsx
@@ -1,8 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { styles } from './style';
+import {
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+  LocationObject
+} from 'expo-location';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+
+  async function requestLocationPermissions() {
+    const { status, granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+
+      setLocation(currentPosition);
+
+      console.log('localização atual', currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
   return (
     <View style={styles.container}>
     </View>
