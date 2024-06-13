fb2834f5d5f8b64833f8853bd5e10cf8ce16c04b
Clepson Lacerda <clepsonsantos9@gmail.com>
2024-06-13 14:47:03 -0300

    add map

--- App.tsx
+++ App.tsx
@@ -4,9 +4,12 @@ import { styles } from './style';
 import {
   getCurrentPositionAsync,
   requestForegroundPermissionsAsync,
-  LocationObject
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy
 } from 'expo-location';
 import { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
 
@@ -29,8 +32,41 @@ export default function App() {
     requestLocationPermissions();
   }, []);
 
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1,
+    }, (response) => {
+      console.log('nova localização', response);
+      setLocation(response);
+
+
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
+
+      {location &&
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
+      }
+
     </View>
   );
 }
